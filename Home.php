9917862df029e21f2ea2b509a1f788ab2c73a111
Shin_Linh <darkprotoss568@gmail.com>
2016-09-20 19:35:43 +1000

    modified Home.php and added logon details for security

--- Home.php
+++ Home.php
@@ -3,25 +3,25 @@
 <body>
 
 <?php
-	$link = mysql_connect('localhost', 'root', 'root');
+	/*require_once("LogonDetails.php");
+	$link = @mysql_connect($host, $user, $password);*/
 ?>
 
 <form method="get">
 Name: <input type="text" name="name" value=""><br>
 Date Of Birth: <input type="date" name="date"><br>
 <?php
-	if($link == true){
+	/*if($link == true){
 		echo "connected";
+		$select_db= mysql_select_db('sde_db');
+	
+		$query = "SELECT * from fullname";
+		$fetch = mysql_query($query) or die ("couldn't find anything");
 	}
 	else
 	{
 		echo"not connected";
-	}
-	$select_db= mysql_select_db('sde_db');
-	
-	$query = "SELECT * from fullname";
-	$fetch = mysql_query($query) or die ("couldn't find anything");
-	
+	}*/
 ?>
 <input type="submit">
 </form>
