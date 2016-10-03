f0df80df182ce27ef6c9fae235ea0839668f9ddb
Shin_Linh <darkprotoss568@gmail.com>
2016-10-03 16:46:51 +1100

    fixed clearDB conn script

--- html/LogonDetais-clearDB.php
+++ html/LogonDetais-clearDB.php
@@ -2,7 +2,7 @@
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $host = $url["host"];
-$user = "$url["host"];
+$user = $url["host"];
 $password = $url["host"];
 $current_db = substr($url["path"], 1);
 /*$current_db = "SDE_db";*/
