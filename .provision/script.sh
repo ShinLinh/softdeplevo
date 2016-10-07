b4e66123e1af1403d36f56de692329e90c7018fe
Shin_Linh <darkprotoss568@gmail.com>
2016-10-07 17:19:11 +1100

    stuff

--- .provision/script.sh
+++ .provision/script.sh
@@ -28,11 +28,20 @@ sudo apt-get -y install apache2
 
 sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password rootpass'
 sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password rootpass'
-sudo apt-get -y install mysql-server libapache2-mod-auth-mysql php5-mysql
+sudo apt-get -y install mysql-server libapache2-mod-auth-mysql #php5-mysql
+
+sudo add-apt-repository -y ppa:ondrej/mysql-5.6
+sudo apt-get update
+sudo apt-get install mysql-server-5.6
 
 # Installing PHP and it's dependencies
-sudo apt-get -y install php5 libapache2-mod-php5 php5-mcrypt
-sudo apt-get -y install php7.0 libapache2-mod-php7.0 php7.0-mysql
+#sudo apt-get -y install php5 libapache2-mod-php5 php5-mcrypt
+sudo apt-get install libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-json
+
+sudo add-apt-repository ppa:ondrej/apache2
+sudo apt-get update
+sudo apt-get install apache2
+
 
 # clean /var/www
 sudo rm -Rf /var/www
