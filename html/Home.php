6625d6c5a2a3ccbebb5617a0d18beb619e84fab3
Jimmy <fricksterx@gmail.com>
2016-10-07 17:22:55 +1100

    create db and table if not found

--- html/Home.php
+++ html/Home.php
@@ -13,12 +13,13 @@
 
 <?php
 	require_once("LogonDetais-clearDB.php");
-	$link = @mysqli_connect($host, $user, $password, $current_db);
+	/*require_once("LogonDetais.php");*/
+	$link = new mysqli($host, $user, $password, $current_db);
 ?>
 <div id="submission">
 	<form id ="sub_info" method="POST">
-		<label>Name:</label> <input type="text" name="fullname"><br>
-		<label>Date Of Birth:</label> <input type="date" name="date"><br>
+		<label>Name:</label> <input type="text" name="fullname" placeholder="Your fullname"><br>
+		<label>Date Of Birth:</label> <input type="date" name="date" placeholder="YYYY/MM/DD"><br>
 		<input type="submit" name ="submit" value="submit"/>
 <?php
 	if($link){
@@ -31,10 +32,32 @@
 			$sql = "CREATE DATABASE SDE_db";
 			
 			if ($link->query($sql) === TRUE) {
-				echo "<p>Table SDE_db created successfully</p>";
+				echo "<p>Database SDE_db created successfully</p>";
 			} else {
-				echo "<p>Error creating table: " . $link->error . "</p>";
+				echo "<p>Error creating Database: " . $link->error . "</p>";
 			}
+			
+			mysqli_query($link,"CREATE TABLE IF NOT EXISTS 'username'(
+						'user_id' INT(10) NOT NULL AUTO_INCREMENT,
+						'fullname' VARCHAR(10) NOT NULL,
+						PRIMARY KEY ('user_id')
+					)
+			") or die(mysql_error);
+			
+			mysqli_query($link,"CREATE TABLE IF NOT EXISTS 'dob'(
+						'dob_id' INT(10) NOT NULL AUTO_INCREMENT,
+						'dob' DATE NOT NULL,
+						PRIMARY KEY ('dob_id')
+					)
+			") or die(mysql_error);
+			
+			mysqli_query($link,"CREATE TABLE IF NOT EXISTS 'subtime'(
+						'subtime_id' INT(10) NOT NULL AUTO_INCREMENT,
+						'submit' TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+						PRIMARY KEY ('subtime_id')
+					)
+			") or die(mysql_error);
+			
 			$link->close();
 		}
 	}
