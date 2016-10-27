39206d835e4c8c76e1bbbe596e609cf0f86d01b2
Jimmy <fricksterx@gmail.com>
2016-10-28 07:50:47 +1100

    result.php works perfectly. another word, version 1 done

--- html/result.php
+++ html/result.php
@@ -12,42 +12,52 @@
 <body>
 <?php
 	//require_once("LogonDetais-clearDB.php");
-	include_once("LogonDetais.php");
 	//$link = new mysqli($host, $user, $password, $current_db);
+	include_once("LogonDetails.php");
 	$link = mysqli_connect($host, $user, $password, $current_db);
-	//$hello = true;
 ?>
 
 
 <div class="wrapper">
+	<p>Version 1</p>
+	<a href="testing.php" class="button_a">submit form page</a>
+	<a href="resultV2.php" class="button_a_overlap">version 2</a>
 <?php
-	 if($link){
-		echo "<p>connected</p>";
-		$result = mysqli_query($link, "SELECT * FROM testdb");
-				
-		echo "<table border='1'>
+	if($link)
+		{
+			$try = "SELECT * FROM testdb";
+			$result = mysqli_query($link, $try);
+			
+		
+		if($result){
+			
+			echo "<table border='1'>
 			<tr>
 				<th>User name</th>
 				<th>Date of Birth</th>
-				<th>submit time</th>
-				<th>user lifetime</th>
+				<th>Submit time</th>
+				<th>User lifetime</th>
 			</tr>";
 				
-		 if ($result->num_rows > 0) {
-			//output data of each row
-			while($row = mysqli_fetch_array($result))
-			{
-				// echo "<tr>";
-				// echo "<td>" . $row['fullname'] . "</td>";
-				// echo "<td>" . $row['dob'] . "</td>";
-				// echo "<td>" . $row['submittime'] . "</td>";
-				// echo "<td>" . $row['lifetime'] . "</td>";
-				// echo "</tr>";
-				echo"<p>lol</p>";
+			if ($result->num_rows > 0) {
+				//output data of each row
+				while($row = mysqli_fetch_array($result))
+				// while($row = mysqli_fetch_array($result))
+				{
+					echo "<tr>";
+					echo "<td>" . $row["fullname"]. "</td>";
+					echo "<td>" . $row["dob"]. "</td>";
+					echo "<td>" . $row["submittime"]. "</td>";
+					echo "<td>" . $row["lifetime"]. "</td>";
+					echo "</tr>";
+				}
 			}
+			else {
+				echo "<p>0 results</p>";
+			}	
 		}
-		else {
-			echo "<p>0 results</p>";
+		else{
+			echo"<p>fail</p>";
 		}
 	}
 	else{
