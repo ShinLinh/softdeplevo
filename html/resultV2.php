cba6e15a857c1e51e5b29a3fb8a61a0d332ae580
Jimmy <fricksterx@gmail.com>
2016-10-30 18:22:00 +1100

    stuff

--- html/resultV2.php
+++ html/resultV2.php
@@ -6,7 +6,6 @@
 		<meta name="keywords" content="Assignment 1"/>
 		<meta name="author" content="Assign1Team"/>
 		<link href= "style.css" rel="stylesheet"/>
-		<!--<script src=".js"></script>-->
 		<title>Result </title>
 	</head> 
 <body>
@@ -19,24 +18,36 @@
 
 
 <div class="wrapper">
-	<p>Version 1</p>
+	<p>Version 2</p>
 	<a href="testing.php" class="button_a">submit form page</a>
-	<a href="resultV2.php" class="button_a_overlap">version 2</a>
+	<a href="result.php" class="button_a_overlap">version 1</a>
 <?php
 	if($link)
 		{
-			$try = "SELECT * FROM testdb";
+			$try = "SELECT user_id FROM testdbV2";
+			$try2= "SELECT user_id FROM testdb";
 			$result = mysqli_query($link, $try);
 			
 		
 		if($result){
 			
+			$mysql_testdb = "CREATE TABLE IF NOT EXISTS testdbV2(
+						user_id INT(100) NOT NULL AUTO_INCREMENT,
+						firstname VARCHAR(30) NOT NULL,
+						lastname VARCHAR(30) NOT NULL,
+						dob VARCHAR(30) NOT NULL,
+						submittime VARCHAR(50) NOT NULL,
+						lifetime INT NOT NULL,
+						PRIMARY KEY (user_id)
+					);";
+			
 			echo "<table border='1'>
 			<tr>
-				<th>User name</th>
+				<th>User first name</th>
+				<th>User last name</th>
 				<th>Date of Birth</th>
 				<th>Submit time</th>
-				<th>User lifetime/day</th>
+				<th>User lifetime</th>
 			</tr>";
 				
 			if ($result->num_rows > 0) {
@@ -45,10 +56,11 @@
 				// while($row = mysqli_fetch_array($result))
 				{
 					echo "<tr>";
-					echo "<td>" . $row["fullname"]. "</td>";
+					echo "<td>" . $row["firstname"]. "</td>";
+					
 					echo "<td>" . $row["dob"]. "</td>";
 					echo "<td>" . $row["submittime"]. "</td>";
-					echo "<td>" . $row["lifetime"]. " day(s)</td>";
+					echo "<td>" . $row["lifetime"]. "</td>";
 					echo "</tr>";
 				}
 			}
