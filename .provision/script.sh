828ecbc3bd20a61ce436cb905b38f8e0b5767e88
ShinLinh <100017631@student.swin.edu.au>
2016-11-03 15:41:03 +1100

    cleaned up provisioning script

--- .provision/script.sh
+++ .provision/script.sh
@@ -1,20 +1,3 @@
-#!/usr/bin/env bash
-
-# nginx
-#sudo apt-get -y install nginx
-#sudo service nginx start
-
-# set up nginx server
-#sudo cp /vagrant/.provision/nginx/nginx.conf /etc/nginx/sites-available/site.conf
-#sudo chmod 644 /etc/nginx/sites-available/site.conf
-#sudo ln -s /etc/nginx/sites-available/site.conf /etc/nginx/sites-enabled/site.conf
-#sudo service nginx restart
-
-# clean /var/www
-#sudo rm -Rf /var/www
-
-# symlink /var/www => /vagrant
-#ln -s /vagrant /var/www
 
 # Updating repository
 
@@ -31,14 +14,13 @@ sudo apt-get -y install apache2
 
 sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password rootpass'
 sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password rootpass'
-sudo apt-get -y install mysql-server libapache2-mod-auth-mysql #php5-mysql
+sudo apt-get -y install mysql-server libapache2-mod-auth-mysql 
 
 sudo add-apt-repository -y ppa:ondrej/mysql-5.6
 sudo apt-get update
 sudo apt-get -y install mysql-server-5.6
 
 # Installing PHP and it's dependencies
-#sudo apt-get -y install php5 libapache2-mod-php5 php5-mcrypt
 sudo apt-get -y install libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-json php7.0-mysqlnd
 
 sudo add-apt-repository ppa:ondrej/apache2
